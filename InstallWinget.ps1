729a0d448a722c8d481ccdc40f12d4965b50d25b
Zinnk <85070802+zinnk@users.noreply.github.com>
2024-12-31 14:42:57 -0300

    Update

---
+++ InstallWinget.ps1
@@ -0,0 +1,63 @@
+# Utilitários de Sistema
+winget install 7zip.7zip
+winget install CPUID.HWMonitor
+winget install CPUID.CPU-Z
+winget install TechPowerUp.GPU-Z
+winget install CrystalDewWorld.CrystalDiskMark
+winget install CrystalDewWorld.CrystalDiskInfo
+winget install JAMSoftware.TreeSize.Free
+winget install AntibodySoftware.WizTree
+winget install -e --id ShareX.ShareX
+winget install voidtools.Everything
+
+# Desenvolvimento
+winget install OpenJS.NodeJS.LTS
+winget install Rustlang.Rust.MSVC
+winget install GoLang.Go
+winget install Python.Python.3.12
+winget install Neovim.Neovim
+winget install Microsoft.VisualStudio.2022.Community
+winget install Microsoft.VisualStudioCode
+winget install Git.Git
+winget install GitHub.cli
+winget install -e --id GitHub.GitHubDesktop
+winget install Postman.Postman
+winget install Yarn.Yarn
+
+# Ferramentas de Desenvolvimento e Design
+winget install DevToys-app.DevToys
+winget install Docker.DockerDesktop
+winget install Parsec.Parsec
+winget install CalcProgrammer1.OpenRGB
+
+# Mídia e Multimídia
+winget install Spotify.Spotify
+winget install VideoLAN.VLC
+winget install Gyan.FFmpeg
+winget install OBSProject.OBSStudio
+winget install MoonlightGameStreamingProject.Moonlight
+winget install Stremio.Stremio
+winget install yt-dlp.yt-dlp
+
+# Produtividade
+winget install Obsidian.Obsidian
+winget install Notepad++.Notepad++
+winget install RevoUninstaller.RevoUninstaller
+winget install Google.GoogleDrive
+winget install Bitwarden.Bitwarden
+winget install Telegram.TelegramDesktop
+winget install Discord.Discord
+winget install GitHub.GitHubDesktop
+
+# Jogos
+winget install Valve.Steam
+winget install EpicGames.EpicGamesLauncher
+winget install HeroicGamesLauncher.HeroicGamesLauncher
+winget install SideQuestVR.SideQuest
+
+# Ferramentas Avançadas
+winget install Anaconda.Anaconda3
+winget install Famatech.AdvancedIPScanner
+winget install Oracle.VirtualBox
+winget install -e --id Canonical.Ubuntu
+wsl --install
